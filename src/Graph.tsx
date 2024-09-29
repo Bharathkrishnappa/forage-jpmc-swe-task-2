0fe33f95b4a53d3d125897d75f4df1767eb510ef
Bharathkrishnappa <149765317+Bharathkrishnappa@users.noreply.github.com>
2024-09-30 00:02:07 +0530

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -27,7 +27,9 @@ class Graph extends Component<IProps, {}> {
   table: Table | undefined;
 
   render() {
-    return React.createElement('perspective-viewer');
+    const elem = React.createElement('perspective-viewer');
+    console.log("Perspective Viewer Element Created: ", elem); // Log the created element
+    return elem;
   }
 
   componentDidMount() {
@@ -41,7 +43,10 @@ class Graph extends Component<IProps, {}> {
     };
 
     if (typeof window.perspective !== 'undefined' && window.perspective.worker()) {
+      console.log("Perspective is loaded"); // Add this line
       this.table = window.perspective.worker().table(schema);
+    } else {
+      console.error("Perspective library not found"); // Add error log
     }
 
     if (this.table) {
@@ -57,11 +62,14 @@ class Graph extends Component<IProps, {}> {
         timestamp: "distinct count"
       }));
     } else {
-      console.error("Perspective not available");
+      console.error("Perspective table not available");
     }
   }
 
   componentDidUpdate(prevProps: IProps) {
+    console.log("Previous Props: ", prevProps.data); // Log previous props data
+    console.log("Current Props: ", this.props.data); // Log current props data
+
     if (this.table) {
       const existingTimestamps = new Set(this.props.data.map(el => el.timestamp));
       const newData = this.props.data.filter((el: any) => !existingTimestamps.has(el.timestamp));
