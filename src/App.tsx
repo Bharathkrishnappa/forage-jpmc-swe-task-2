bf82c1f547c35cf9b6f4e9bf7728ad74be6a1069
Bharathkrishnappa <149765317+Bharathkrishnappa@users.noreply.github.com>
2024-09-30 00:01:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,8 @@ class App extends Component<{}, IState> {
     let x = 0;
     const interval = setInterval(() => {
       DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        console.log("Server Responds: ", serverResponds); // Add this line
+
         const uniqueData = serverResponds.filter((item, index, self) =>
           index === self.findIndex((t) => (
             t.timestamp === item.timestamp && t.stock === item.stock
@@ -52,7 +54,7 @@ class App extends Component<{}, IState> {
         this.setState((prevState) => ({
           data: [...prevState.data, ...uniqueData],
           showGraph: true,
-        })); // Fixed closing parentheses
+        }));
       });
       x++;
       if (x > 1000) {
